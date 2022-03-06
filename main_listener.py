49e107996ad75d2bcd20755103c99b3a478d9cca
Algo Rhythm <VaultStaking@gmail.com>
2022-03-06 14:07:07 -0500

    listener

--- main_listener.py
+++ main_listener.py
@@ -1,19 +1,14 @@
-import json
-import urllib.request
-
 from discord.ext.commands import MissingRequiredArgument, MissingRole, BadArgument, CommandInvokeError
 
-from logger import raw_audit_log, log
+from logger import raw_audit_log
 
 import discord
-from discord.ext import commands, tasks
+from discord.ext import commands
 
 import secrets
-from checkpoint_db import get_latest_saved_checkpoint, get_last_validator_checkpoint, \
-    update_validator_checkpoint, set_new_checkpoint, send_email
 from validator_db import get_val_name_from_id, get_val_contacts_from_id, get_val_uptime_from_id, \
     get_val_missed_latest_checkpoint_from_id, get_val_commission_percent_from_id, get_val_self_stake_from_id, \
-    get_val_delegated_stake_from_id, set_val_contacts_from_id, remove_val_contacts_from_id
+    get_val_delegated_stake_from_id, set_val_contacts_from_id, remove_val_contacts_from_id, get_db_connection
 
 token = secrets.DISCORD_TOKEN
 bot = commands.Bot(command_prefix='$')
@@ -29,12 +24,13 @@ async def on_ready():
 @bot.command(name='status', help='$status [validator id]')
 @commands.has_any_role(*secrets.LISTENER_ROLES)
 async def status(ctx, val_id: int):
-
-    no_of_checkpoints = (100-get_val_uptime_from_id(str(val_id)))*2
-    message = get_val_name_from_id(str(val_id)) + " has missed " + str(no_of_checkpoints) + " out of the last 200 checkpoints. \n" \
-              "Validator has " + ("not " if get_val_missed_latest_checkpoint_from_id(str(val_id)) > 0 else "") + \
+    db_connection = get_db_connection()
+    no_of_checkpoints = (100-get_val_uptime_from_id(db_connection, str(val_id)))*2
+    message = get_val_name_from_id(db_connection, str(val_id)) + " has missed " + str(no_of_checkpoints) + " out of the last 200 checkpoints. \n" \
+              "Validator has " + ("not " if get_val_missed_latest_checkpoint_from_id(db_connection, str(val_id)) > 0 else "") + \
               "signed the latest checkpoint."
     await ctx.send(message)
+    db_connection.close()
 
 
 @bot.command(name='missed', help='$missed')
@@ -51,27 +47,33 @@ async def missed(ctx):
 @bot.command(name='details', help='$details [validator id]')
 @commands.has_any_role(*secrets.LISTENER_ROLES)
 async def details(ctx, val_id: str):
-    embed = discord.Embed(title= get_val_name_from_id(str(val_id)),
+    db_connection = get_db_connection()
+
+    embed = discord.Embed(title= get_val_name_from_id(db_connection, str(val_id)),
                           color=discord.Color.blue())
-    embed.add_field(name="Contacts", value=get_val_contacts_from_id(str(val_id)), inline=False)
-    embed.add_field(name="Commission", value=(str(get_val_commission_percent_from_id(str(val_id))) + "%"), inline=False)
-    embed.add_field(name="Self Stake", value="{:,.2f}".format(float(get_val_self_stake_from_id(str(val_id)))/1e18), inline=False)
-    embed.add_field(name="Delegated Stake", value="{:,.2f}".format(float(get_val_delegated_stake_from_id(str(val_id)))/1e18), inline=False)
-    embed.add_field(name="Uptime", value=(str(get_val_uptime_from_id(str(val_id))) + "%"), inline=False)
+    embed.add_field(name="Contacts", value=get_val_contacts_from_id(db_connection, str(val_id)), inline=False)
+    embed.add_field(name="Commission", value=(str(get_val_commission_percent_from_id(db_connection, str(val_id))) + "%"), inline=False)
+    embed.add_field(name="Self Stake", value="{:,.2f}".format(float(get_val_self_stake_from_id(db_connection, str(val_id)))/1e18), inline=False)
+    embed.add_field(name="Delegated Stake", value="{:,.2f}".format(float(get_val_delegated_stake_from_id(db_connection, str(val_id)))/1e18), inline=False)
+    embed.add_field(name="Uptime", value=(str(get_val_uptime_from_id(db_connection, str(val_id))) + "%"), inline=False)
 
     await ctx.send(embed=embed)
+    db_connection.close()
 
 
 @bot.command(name='contacts', help='$contacts [validator id]')
 @commands.has_any_role(*secrets.LISTENER_ROLES)
 async def contacts(ctx, val_id: str):
-    message = get_val_name_from_id(str(val_id)) + " has the following contacts: " + get_val_contacts_from_id(str(val_id))
+    db_connection = get_db_connection()
+    message = get_val_name_from_id(db_connection, str(val_id)) + " has the following contacts: " + \
+              get_val_contacts_from_id(db_connection, str(val_id))
     await ctx.send(message)
 
 
-@bot.command(name='contacts-add', help='$contacts-add [validator id] @user1 (@user2 @user3...)')
+@bot.command(name='contacts-add', help='$contacts-add [validator id] @user1 (@user2...)')
 @commands.has_any_role(*secrets.LISTENER_ROLES)
 async def contacts_add(ctx, val_id: int, user_one, user_two="", user_three="", user_four="", user_five=""):
+    db_connection = get_db_connection()
     contact = user_one
     if user_two != "":
         contact += ", " + user_two
@@ -82,22 +84,25 @@ async def contacts_add(ctx, val_id: int, user_one, user_two="", user_three="", u
     if user_five != "":
         contact += ", " + user_five
 
-    set_val_contacts_from_id(str(val_id), contact)
-    message = get_val_name_from_id(str(val_id)) + " now has the following contacts: " + get_val_contacts_from_id(
-        str(val_id))
+    set_val_contacts_from_id(db_connection, str(val_id), contact)
+    message = get_val_name_from_id(db_connection, str(val_id)) + " now has the following contacts: " + \
+              get_val_contacts_from_id(db_connection, str(val_id))
     await ctx.send(message)
+    db_connection.close()
     return
 
 
-@bot.command(name='contacts-remove', help='$contacts-remove [validator id] @user1 (@user2 @user3...)')
+@bot.command(name='contacts-remove', help='$contacts-remove [validator id] @user1 (@user2...)')
 @commands.has_any_role(*secrets.LISTENER_ROLES)
 async def contacts_remove(ctx, val_id: int, *users):
+    db_connection = get_db_connection()
     for user in users:
-        remove_val_contacts_from_id(str(val_id), user)
-        message = get_val_name_from_id(str(val_id)) + " now has the following contacts: " + get_val_contacts_from_id(
-            str(val_id))
+        remove_val_contacts_from_id(db_connection, str(val_id), user)
+        message = get_val_name_from_id(db_connection, str(val_id)) + " now has the following contacts: " + \
+                  get_val_contacts_from_id(db_connection, str(val_id))
         await ctx.send(message)
-        return
+    db_connection.close()
+    return
 
 
 #### Error methods ####
@@ -189,11 +194,11 @@ async def contacts_add_error(ctx, error):
         raw_audit_log(str(error))
         raise error
     elif isinstance(error, BadArgument):
-        await ctx.send("usage: `$contacts-add [validator id] @user1 (@user2 @user3...)`. \n"
+        await ctx.send("usage: `$contacts-add [validator id] @user1 (@user2...)`. \n"
                        "There was a bad argument.")
         raise error
     elif isinstance(error, MissingRequiredArgument):
-        await ctx.send("Missing argument, try: `$contacts-add [validator id] @user1 (@user2 @user3...)`")
+        await ctx.send("Missing argument, try: `$contacts-add [validator id] @user1 (@user2...)`")
         raise error
     elif isinstance(error, MissingRole):
         await ctx.send("Role '" + secrets.LISTENER_ROLES + "' is required to run this command.")
@@ -209,11 +214,11 @@ async def contacts_remove_error(ctx, error):
         raw_audit_log(str(error))
         raise error
     elif isinstance(error, BadArgument):
-        await ctx.send("usage: `$contacts-remove [validator id] @user1 (@user2 @user3...)`. \n"
+        await ctx.send("usage: `$contacts-remove [validator id] @user1 (@user2...)`. \n"
                        "There was a bad argument.")
         raise error
     elif isinstance(error, MissingRequiredArgument):
-        await ctx.send("Missing argument, try: `$contacts-remove [validator id] @user1 (@user2 @user3...)`")
+        await ctx.send("Missing argument, try: `$contacts-remove [validator id] @user1 (@user2...)`")
         raise error
     elif isinstance(error, MissingRole):
         await ctx.send("Role '" + secrets.LISTENER_ROLES + "' is required to run this command.")
